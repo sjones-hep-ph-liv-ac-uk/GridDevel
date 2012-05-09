aef0fba3119d91e586f628202ebdb0f229f7eaba
Steve Jones <sjones@hep.ph.liv.ac.uk>
2012-05-09 16:41:31 +0100

    add

---
+++ vs_scripts/doit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./VomsSnooper.sh --xmlfile VOIDCardInfo.xml --myvos myvos.txt --vodfile vod.txt --voddir vo.d --outfile vo-info.def --extrafields --contactsoutfile contacts.txt --vomsdir vomsdir
