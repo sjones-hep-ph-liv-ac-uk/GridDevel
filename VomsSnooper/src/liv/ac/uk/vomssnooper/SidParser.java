f9d996c96a78159f0980b35ceb6c174ecc252e08
Steve Jones <sjones@hep.ph.liv.ac.uk>
2012-05-10 13:46:56 +0100

    tmp commit

--- VomsSnooper/src/liv/ac/uk/vomssnooper/SidParser.java
+++ VomsSnooper/src/liv/ac/uk/vomssnooper/SidParser.java
@@ -1,11 +1,20 @@
 package liv.ac.uk.vomssnooper;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class SidParser {
+	
+	private String oldSidDir;
+	private ArrayList<VirtOrgInfo> allVoidInfo;
 
-	public SidParser(String oldSidDir, ArrayList<VirtOrgInfo> voidInfo) {
-		// TODO Auto-generated constructor stub
+	public SidParser(String os, ArrayList<VirtOrgInfo> v) {
+		oldSidDir = os;
+		allVoidInfo = v;
 	}
 	
 	/**
@@ -13,6 +22,24 @@ public class SidParser {
 	 * @return null
 	 */
 	public void parseDocument() {
+		try {
+
+			FileInputStream fstream = new FileInputStream(new File (oldSidDir + "/site-info.def"));
+
+			DataInputStream in = new DataInputStream(fstream);
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+
+			while ((strLine = br.readLine()) != null) {
+				System.out.print(strLine);
+			}
+			in.close();
+		} catch (Exception e) {// Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+			System.exit(1);
+		}
+
 	}
 	
 
