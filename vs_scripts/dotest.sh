21e7154738cacbae955a52266720c2137c35e7f6
Steve Jones <sjones@hep.ph.liv.ac.uk>
2012-05-16 16:36:46 +0100

    Add test file

---
+++ vs_scripts/dotest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget -O VOIDCardInfo.xml http://operations-portal.egi.eu/xml/voIDCard/public/all/true
+
+cat  VOIDCardInfo.xml | sed -e "s%</VoDump>%%" > deleteme1
+cat  ExtraVOIDCardInfo.xml | grep -v "^<VoDump>"  > deleteme2
+
+cat deleteme1 deleteme2 > VOIDCardInfo.xml
+
+ant clean
+ant
+
+./VomsSnooper.sh --xmlfile VOIDCardInfo.xml  --myvos myvos.txt --vodfile vod.txt --voddir /user2/sjones/git/GridDevel/vs_scripts/test/xml/vo.d --outfile /user2/sjones/git/GridDevel/vs_scripts/test/xml/site-info.def
+
+./SidFormatter.sh --oldsiddir test/org/ --newsiddir test/new/
+
+
