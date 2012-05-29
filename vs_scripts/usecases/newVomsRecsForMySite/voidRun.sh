828d1c6a669a87752e91f015b3a256cef376d6da
Steve Jones <sjones@hep.ph.liv.ac.uk>
2012-05-29 14:22:26 +0100

    Fixed path

--- vs_scripts/usecases/newVomsRecsForMySite/voidRun.sh
+++ vs_scripts/usecases/newVomsRecsForMySite/voidRun.sh
@@ -29,7 +29,7 @@ cat  ExtraVOIDCardInfo.xml | grep -v "^<VoDump>"  > deleteme2
 cat deleteme1 deleteme2 > VOIDCardInfo.xml
 
 # Convert it into sidvod format
-java -jar ./dist/lib/VomsSnooper.jar --xmlfile VOIDCardInfo.xml  --myvos void/myvos.txt --vodfile void/vod.txt --voddir /user2/sjones/git/GridDevel/vs_scripts/void/xml/vo.d --outfile /user2/sjones/git/GridDevel/vs_scripts/void/xml/site-info.def
+java -jar ./dist/lib/VomsSnooper.jar --xmlfile VOIDCardInfo.xml  --myvos void/myvos.txt --vodfile void/vod.txt --voddir ~/git/GridDevel/vs_scripts/usecases/newVomsRecsForMySite/void/xml/vo.d --outfile /user2/sjones/git/GridDevel/vs_scripts/usecases/newVomsRecsForMySite/void/xml/site-info.def
 
 ############################################
 # Section 2 - Get the operational data,    #
@@ -38,7 +38,7 @@ java -jar ./dist/lib/VomsSnooper.jar --xmlfile VOIDCardInfo.xml  --myvos void/my
 ############################################
 #
 # Transfer from hepgrid6 via:
-rsync -av --delete root@hepgrid6:/root/glitecfg/ /user2/sjones/git/GridDevel/vs_scripts/void/deployed
+rsync -av --delete root@hepgrid6:/root/glitecfg/ /user2/sjones/git/GridDevel/vs_scripts/usecases/newVomsRecsForMySite/void/deployed
 
 ./merger.pl --oldsiddir void/deployed --deltadir void/xml/ --newsiddir  void/merged
 
