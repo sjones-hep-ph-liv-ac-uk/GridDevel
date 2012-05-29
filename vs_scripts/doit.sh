029e28b25797ea7eba939d198da9df084b0aa516
Steve Jones <sjones@hep.ph.liv.ac.uk>
2012-05-29 13:59:28 +0100

    Get rid of more cruft

--- vs_scripts/doit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./VomsSnooper.sh --xmlfile VOIDCardInfo.xml --myvos myvos.txt --vodfile vod.txt --voddir vo.d --outfile vo-info.def --extrafields --contactsoutfile contacts.txt --vomsdir vomsdir
