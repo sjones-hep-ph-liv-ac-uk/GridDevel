3939a71d0a2bf63f5dc3ba89e1d80448a8a85ef8
Steve Jones <sjones@hep.ph.liv.ac.uk>
2012-05-29 13:49:20 +0100

    Cleanup

--- vs_scripts/testScript1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget -O VOIDCardInfo.xml http://operations-portal.egi.eu/xml/voIDCard/public/all/true
-
-./VomsSnooper.sh --xmlfile VOIDCardInfo.xml  --myvos myvos.txt --vodfile vod.txt --voddir vo.d --outfile site-info.def 
-
-./SidFormatter.sh --oldsiddir . --newsiddir newsitedir
-
