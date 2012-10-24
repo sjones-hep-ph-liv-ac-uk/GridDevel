41d257f417c1a6575aa81c5318164ff227a154e1
Steve Jones <sjones@hep.ph.liv.ac.uk>
2012-10-24 14:55:00 +0100

    javadoc

--- VomsSnooper/src/liv/ac/uk/vomssnooper/Fqan.java
+++ VomsSnooper/src/liv/ac/uk/vomssnooper/Fqan.java
@@ -9,48 +9,90 @@ package liv.ac.uk.vomssnooper;
 
 public class Fqan {
 
-  private String groupType;
-	private String fqanExpr;
-	private String description;
-	private Boolean isUsed;
-	
-	
-	public Fqan()
-	{
+	private String groupType; // type
+	private String fqanExpr; // fqan
+	private String description; // desc
+	private Boolean isUsed; // whether used
+
+	/**
+	 * Constructor
+	 */
+	public Fqan() {
 		groupType = "dummy";
 		fqanExpr = "dummy";
 		description = "dummy";
-		isUsed= false;
+		isUsed = false;
 	}
-	
+
+	/**
+	 * Is this used
+	 * 
+	 * @return
+	 */
 	public Boolean getIsUsed() {
 		return isUsed;
 	}
+
+	/**
+	 * Set whether used
+	 * 
+	 * @param isUsed
+	 */
 	public void setIsUsed(Boolean isUsed) {
 		this.isUsed = isUsed;
 	}
+
+	/**
+	 * get the type
+	 * 
+	 * @return
+	 */
 	public String getGroupType() {
 		return groupType;
 	}
 
+	/**
+	 * set the type
+	 * 
+	 * @param groupType type string
+	 */
 	public void setGroupType(String groupType) {
 		this.groupType = groupType;
 	}
 
+	/**
+	 * get the fqan
+	 * 
+	 * @return
+	 */
 	public String getFqanExpr() {
 		return fqanExpr;
 	}
 
+	/**
+	 * Set the fqan
+	 * 
+	 * @param fqanExpr fqan
+	 */
 	public void setFqanExpr(String fqanExpr) {
 		this.fqanExpr = fqanExpr;
 	}
 
+	/**
+	 * Get the desc
+	 * 
+	 * @return
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * Set the desc
+	 * 
+	 * @param description desc
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
 }
-
