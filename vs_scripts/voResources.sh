f50cd094ad43e1dfdb173e318f9e45a131cbd595
Steve Jones <sjones@hep.ph.liv.ac.uk>
2013-02-08 15:15:59 +0000

    Add wrappers

---
+++ vs_scripts/voResources.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ ! -x $VS_JAR_DIR/VoResources.jar ]; then
+  echo Somehow, this program has been installed poorly.
+  echo You could try setting the VS_JAR_DIR directory
+  echo to point to the location of the correct JAR files.
+  exit 1
+fi
+
+java -jar $VS_JAR_DIR/VoResources.jar "$@"
+
