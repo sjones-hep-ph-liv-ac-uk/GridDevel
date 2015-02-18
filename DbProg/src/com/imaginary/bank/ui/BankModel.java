9001fbee6580327c6aea8786f005c7ed57d4470d
Stephen Jones <sjones@hep169.ph.liv.ac.uk>
2015-02-18 13:48:41 +0000

    get rid of the db prog files

--- DbProg/src/com/imaginary/bank/ui/BankModel.java
+++
@@ -1,9 +0,0 @@
-package com.imaginary.bank.ui;
-
-import javax.swing.tree.DefaultTreeModel;
-
-public class BankModel extends DefaultTreeModel {
-    public BankModel() {
-        super(new RootNode());
-    }
-}
--- DbProg/src/com/imaginary/lwp/Session.java
+++
@@ -1,7 +0,0 @@
-package com.imaginary.lwp;
-
-import java.rmi.Remote;
-
-public interface Session extends Remote {
-    
-}
